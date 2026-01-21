c130696d727369e8233fc0eb0fe458f9e044ab8a
Antony Muuo <tonniehmuas@gmail.com>
2026-01-21 11:25:41 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2397588411-6a237",
+  "appId": "1:164155380732:web:2100ce12b7b23b42699879",
+  "apiKey": "AIzaSyAiofv-N1lmmtk4rCUX_d2DFY_5j2OH1yc",
+  "authDomain": "studio-2397588411-6a237.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "164155380732"
+};
