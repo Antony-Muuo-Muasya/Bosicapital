70ffd19879994bf75e9be23aa50432487e97c654
Antony Muuo <tonniehmuas@gmail.com>
2026-01-23 04:50:59 +0000

    i am getting the error "Password is required" even after entering the pa

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -138,7 +138,7 @@ export default function SignupPage() {
                   <FormItem>
                     <FormLabel>Password</FormLabel>
                     <FormControl>
-                      <Input type="password" autoComplete="new-password" />
+                      <Input type="password" {...field} autoComplete="new-password" />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
