2306cac6968bda5d7247bdd63d6190b92258ed14
Antony Muuo <tonniehmuas@gmail.com>
2026-02-12 04:36:33 +0000

    i want to be able to update the brand logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,15 +4,9 @@ export const BosiCapitalLogo = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 24 24"
-    fill="none"
-    stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
+    fill="currentColor"
     {...props}
   >
-    <path d="M12 2L2 7l10 5 10-5-10-5z" />
-    <path d="M2 17l10 5 10-5" />
-    <path d="M2 12l10 5 10-5" />
+    <path d="M13.5,5.5A3.5,3.5 0 0,1 17,9A3.5,3.5 0 0,1 13.5,12.5H10V5.5H13.5M13,14.5H10V18.5H13A2.5,2.5 0 0,0 15.5,16A2.5,2.5 0 0,0 13,14.5M8,2V22H13.5C16.5,22 19,19.5 19,16.5C19,14.36 17.64,12.61 15.7,12.09C17.15,11.53 18,10.14 18,8.5C18,5.47 15.53,3 12.5,3H8V2Z" />
   </svg>
 );
