e70ba703fa95f362e4cec5124f84cf6953f10464
Antony Muuo <tonniehmuas@gmail.com>
2026-02-18 16:08:28 +0000

    add this code on index.html file "<script type="text/javascript">     (f

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,29 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=PT+Sans:wght@400;700&display=swap"
           rel="stylesheet"
         />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function (l) {
+      if (l.search[1] === "/") {
+        var decoded = l.search
+          .slice(1)
+          .split("&")
+          .map(function (s) {
+            return s.replace(/~and~/g, "&");
+          })
+          .join("?");
+        window.history.replaceState(
+          null,
+          null,
+          l.pathname.slice(0, -1) + decoded + l.hash
+        );
+      }
+    })(window.location);
+  `,
+          }}
+        />
       </head>
       <body className="font-body antialiased">
         <ThemeProvider
